20b989101ec76609684c6668d83e1f0269ed10b5
Nyasha Zishiri <23294558@sun.ac.za>
2025-03-31 20:48:58 +0200

    Project card and skills card made reusable

--- client/src/components/ProjectCard.tsx
+++ client/src/components/ProjectCard.tsx
@@ -1,27 +1,63 @@
 import '../styles/project_card.css'
 
-export function ProjectCard() {
+interface ProjectCardProps {
+    image: string;
+    title: string;
+    description: string;
+    technologies: string;
+    codeLink: string;
+    demoLink: string;
+}
+
+export function ProjectCard({image, title, description, technologies, codeLink, demoLink}: ProjectCardProps) {
+    if (!image) {
+        image = "https://cdn.pixabay.com/photo/2021/04/24/06/30/sunset-6203315_640.png";
+    }
+
+    if (!title) {
+        title = "Project Title";
+    }
+
+    if (!description) {
+        description = "Project Description";
+    }
+
+    if (!technologies) {
+        technologies = "React Node Express MongoDB React Node Express MongoDB React Node";
+    }
+
+    if (!codeLink) {
+        codeLink = "https://github.com";
+    }
+
+    if (!demoLink) {
+        demoLink = "https://github.com";
+    }
+
+
     return (
         <>
             <div className="project-card">
                 <img className="project-image"
-                     src="https://cdn.pixabay.com/photo/2021/04/24/06/30/sunset-6203315_640.png"/>
+                     src={image}/>
                 <div className="project-content">
-                    <h2 className="project-title">Project Title</h2>
+                    <h2 className="project-title">{title}</h2>
                     <span className="project-description">
-                                lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros
-                                elementum tristique.em ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                                varius enim in eros.
-                            </span>
+                                {description}
+                    </span>
 
-                    <h2 className="project-subtitle">Technologies Used:</h2>
+                    <h2 className="project-subtitle">Technologies:</h2>
                     <span className="project-tools">
-                                React Node Express MongoDB React Node Express MongoDB React Node
+                                {technologies}
                             </span>
 
                     <div className="project-links">
-                        <button className="project-button">View Code</button>
-                        <button className="project-button">Live Demo</button>
+                        <button className="project-button"
+                                onClick={() => window.open(codeLink, "_blank")}>View Code
+                        </button>
+                        <button className="project-button"
+                                onClick={() => window.open(demoLink, "_blank")}>Live Demo
+                        </button>
                     </div>
                 </div>
             </div>
--- client/src/components/SkillsCard.tsx
+++ client/src/components/SkillsCard.tsx
@@ -1,16 +1,36 @@
 import '../styles/skill_card.css'
+import {ReactNode} from "react";
 
-export function SkillsCard() {
+interface SkillCardProps {
+    icon: string;
+    category: string;
+    skills: ReactNode[];
+}
+
+export function SkillsCard({icon, category, skills}: SkillCardProps) {
+    if (!icon) {
+        icon = "https://img.icons8.com/color/48/000000/html-5.png";
+    }
+
+    if (!category) {
+        category = "Category";
+    }
+
+    if (!skills) {
+        skills = ["Skill 1", "Skill 2", "Skill 3", "Skill 4"];
+    }
+
     return (
         <>
             <div className="skills-card">
-                <img className="skills-icon" src="https://img.icons8.com/color/48/000000/html-5.png"/>
-                <h2 className="skills-title">Frontend</h2>
+                <img className="skills-icon" src={icon}/>
+                <h2 className="skills-title">{category}</h2>
                 <ul className="skills-list">
-                    <li>React</li>
-                    <li>Node</li>
-                    <li>Express</li>
-                    <li>MongoDB</li>
+                    {
+                        skills.map((skill, index) => (
+                            <li key={index}>{skill}</li>
+                        ))
+                    }
                 </ul>
             </div>
         </>
