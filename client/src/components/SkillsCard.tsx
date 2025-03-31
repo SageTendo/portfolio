5ce3989acfcb4840f49a70826cce03a632339971
Nyasha Zishiri <23294558@sun.ac.za>
2025-03-31 21:16:27 +0200

    Skills card revamp

--- client/src/components/SkillsCard.tsx
+++ client/src/components/SkillsCard.tsx
@@ -1,10 +1,15 @@
 import '../styles/skill_card.css'
 import {ReactNode} from "react";
 
+type Skill = {
+    icon: string;
+    skill: string
+}
+
 interface SkillCardProps {
     icon: string;
     category: string;
-    skills: ReactNode[];
+    skills: Skill[];
 }
 
 export function SkillsCard({icon, category, skills}: SkillCardProps) {
@@ -17,18 +22,27 @@ export function SkillsCard({icon, category, skills}: SkillCardProps) {
     }
 
     if (!skills) {
-        skills = ["Skill 1", "Skill 2", "Skill 3", "Skill 4"];
+        skills = [
+            {"icon": "https://img.icons8.com/color/48/000000/html-5.png", "skill": "HTML"},
+            {"icon": "https://img.icons8.com/color/48/000000/css3.png", "skill": "CSS"},
+            {"icon": "https://img.icons8.com/color/48/000000/javascript.png", "skill": "JavaScript"},
+            {"icon": "https://img.icons8.com/color/48/000000/typescript.png", "skill": "React"},
+        ];
     }
 
     return (
         <>
             <div className="skills-card">
-                <img className="skills-icon" src={icon}/>
                 <h2 className="skills-title">{category}</h2>
                 <ul className="skills-list">
                     {
                         skills.map((skill, index) => (
-                            <li key={index}>{skill}</li>
+                            <li key={index} className="skills-item">
+                                <img src={skill.icon}
+                                     width="32"
+                                     height="32"/>
+                                {skill.skill}
+                            </li>
                         ))
                     }
                 </ul>
