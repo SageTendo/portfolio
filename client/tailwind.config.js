232c76d6dd1b3a340ad7f350feda4322f870c972
SageTendo <39864316+SageTendo@users.noreply.github.com>
2025-07-28 05:42:31 +0200

    feat: Tailwind migration

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,html}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['system-ui', 'Avenir', 'Helvetica', 'Arial', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
