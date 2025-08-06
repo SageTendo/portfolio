002873190042c30945e311099fef1fc6b0b42a51
SageTendo <39864316+SageTendo@users.noreply.github.com>
2025-08-06 15:36:06 +0200

    using env vars

--- client/src/lib/api/Base.ts
+++ client/src/lib/api/Base.ts
@@ -1,6 +1,4 @@
-const BaseUrl = "https://sagetendo-portfolio.vercel.app/api";
-// const BaseUrl = "http://localhost:8000/api";
-
+const BaseUrl = import.meta.env.VITE_API_ENDPOINT;
 export const Client = {
   get: async (path: string) => {
     const response = await fetch(BaseUrl + path);
